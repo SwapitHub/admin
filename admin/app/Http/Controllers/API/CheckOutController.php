e79ecc476024f6b6a1e7935f019e245d7d332249
Shubham sharma <shubham.swapit@gmail.com>
2024-05-27 12:12:14 +0530

    added charge api

--- admin/app/Http/Controllers/API/CheckOutController.php
+++ admin/app/Http/Controllers/API/CheckOutController.php
@@ -177,6 +177,10 @@ class CheckOutController extends Controller
             'email'=>$userEmail,
         ];
         $response =  $this->createCharge($chargeData);
+
+        var_dump($response);
+        exit;
+
         if($response['res'] =='success')
         {
             $orderData['ref_num'] = $response['ref_num'];
