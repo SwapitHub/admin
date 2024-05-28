8f537fe63cdb81480a94f77efa35c239c7c60b48
Shubham sharma <shubham.swapit@gmail.com>
2024-05-28 10:12:15 +0530

    modified home content api

--- admin/app/Http/Controllers/API/SiteinfoController.php
+++ admin/app/Http/Controllers/API/SiteinfoController.php
@@ -37,6 +37,8 @@ class SiteinfoController extends Controller
         $output['msg'] = 'data retrieved successfully';
         $cacheKey = 'site_info';
         $siteinfo = Cache::get($cacheKey);
+        $siteinfo = Cache::forget($cacheKey);
+        exit;
         if (!$siteinfo) {
         $collection = [];
         $section1 =  HomeSection1::first();
