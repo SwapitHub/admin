91cad9eeb3d758e031af3695d6760d975ffa4ade
shubham sharma <shubham.swapit@gmail.com>
2024-06-04 17:02:14 +0530

    modified banners

--- admin/app/Http/Controllers/API/OrdersController.php
+++ admin/app/Http/Controllers/API/OrdersController.php
@@ -31,7 +31,7 @@ class OrdersController extends Controller
             return response()->json($output, 401);
         } else {
             $user_id = $request->user_id;
-            $orders = OrderModel::orderBy('id','desc')->where('user_id', $user_id)->paginate(5);
+            $orders = OrderModel::orderBy('id', 'desc')->where('user_id', $user_id)->paginate(5);
             $output['res'] = 'success';
             $output['msg'] = 'Orders are ..';
             $output['data'] = $orders;
@@ -56,8 +56,7 @@ class OrdersController extends Controller
             return response()->json($output, 401);
         } else {
 
-            if(!OrderModel::where('order_id',$request->order_id)->first())
-            {
+            if (!OrderModel::where('order_id', $request->order_id)->first()) {
                 $output['res'] = 'error';
                 $output['msg'] = 'Invalid OrderId';
                 $output['data'] = [];
@@ -67,12 +66,10 @@ class OrdersController extends Controller
 
             $collection_arr = [];
             $orders = OrderItem::where('order_id', $request->order_id)->get();
-            foreach ($orders as $order)
-            {
+            foreach ($orders as $order) {
                 $order_data = json_decode($order->order_data);
                 // fetch ring details for order
-                if(!empty($order_data->ring_id))
-                {
+                if (!empty($order_data->ring_id)) {
                     $ring_details_arr = [];
                     $ring_data = ProductModel::find($order_data->ring_id);
                     $ring_details_arr['ring_name'] = $ring_data['name'];
@@ -80,34 +77,31 @@ class OrdersController extends Controller
                     $ring_details_arr['ring_size'] = $order_data->ring_size;
                     $ring_details_arr['ring_price'] = $order_data->ring_price;
                     // get product Image
-                    $ring_details_arr['ring_image'] = getProductImages($order_data->ring_id,$order_data->ring_color);
+                    $ring_details_arr['ring_image'] = getProductImages($order_data->ring_id, $order_data->ring_color);
 
                     $order->ring_detail = json_encode($ring_details_arr);
-                }else{
+                } else {
                     $order->ring_detail = null;
                 }
                 // fetch diamond details for order
-                if(!empty($order_data->diamond_id) && !empty($order_data->diamond_type))
-                {
-                   $diamond_details_arr = [];
-                   $diamond_details =  getDiamondImages($order_data->diamond_id,$order_data->diamond_type);
-                   $diamond_details_arr['diamond_image'] = $diamond_details->image_url;
-                   $diamond_details_arr['stock_number']  = $order_data->diamond_id;
-                   $diamond_details_arr['diamond_type']  = $order_data->diamond_type;
-                   $diamond_details_arr['diamond_carat'] = $diamond_details->size.' Carat';
-                   $diamond_details_arr['diamond_shape'] = $diamond_details->shape;
-                   $diamond_details_arr['diamond_color']  = $diamond_details->color;
-                   $diamond_details_arr['diamond_clarity'] = $diamond_details->clarity;
-                   $diamond_details_arr['diamond_cut'] = $diamond_details->cut;
-                   $diamond_details_arr['diamond_price'] = $order_data->diamond_price;
-                   $order->diamond_detail = json_encode($diamond_details_arr);
-                }
-                else{
+                if (!empty($order_data->diamond_id) && !empty($order_data->diamond_type)) {
+                    $diamond_details_arr = [];
+                    $diamond_details =  getDiamondImages($order_data->diamond_id, $order_data->diamond_type);
+                    $diamond_details_arr['diamond_image'] = $diamond_details->image_url;
+                    $diamond_details_arr['stock_number']  = $order_data->diamond_id;
+                    $diamond_details_arr['diamond_type']  = $order_data->diamond_type;
+                    $diamond_details_arr['diamond_carat'] = $diamond_details->size . ' Carat';
+                    $diamond_details_arr['diamond_shape'] = $diamond_details->shape;
+                    $diamond_details_arr['diamond_color']  = $diamond_details->color;
+                    $diamond_details_arr['diamond_clarity'] = $diamond_details->clarity;
+                    $diamond_details_arr['diamond_cut'] = $diamond_details->cut;
+                    $diamond_details_arr['diamond_price'] = $order_data->diamond_price;
+                    $order->diamond_detail = json_encode($diamond_details_arr);
+                } else {
                     $order->diamond_detail = null;
                 }
                 // fetch gemstone details for order
-                if(!empty($order_data->gemstone_id))
-                {
+                if (!empty($order_data->gemstone_id)) {
                     $gemstone_details_arr = [];
                     $gemstone_details = getGemStoneImages($order_data->gemstone_id);
                     $gemstone_details_arr['gemstone_image'] = $gemstone_details->image_url;
@@ -116,31 +110,43 @@ class OrdersController extends Controller
                     $gemstone_details_arr['gemstone_price'] = $order_data->gemstone_price;
 
                     $order->gemstone_detail = json_encode($gemstone_details_arr);
-                }
-                else{
+                } else {
                     $order->gemstone_detail = null;
                 }
 
                 $collection_arr[] = $order;
-
             }
-            $initial_order = OrderModel::where('order_id',$request->order_id)->first();
+            $initial_order = OrderModel::where('order_id', $request->order_id)->first();
             $address_count = $initial_order->address;
-            $address_ =  explode(',',$address_count);
-            $addressToSend = [];
-            foreach($address_ as $adr)
-            {
-                $adr = AddresModel::where('id',$adr)->first();
-                $addressToSend[] = $adr;
+            $address_ =  explode(',', $address_count);
+            // $addressToSend = [];
+            $shipping_address = '';
+            $billing_address = '';
+            foreach ($address_ as $adr) {
+                $adr = AddresModel::where('id', $adr)->first();
+                if ($adr->type == 'both') {
+                    $billing_address = $adr;
+                    $shipping_address = $adr;
+                }
+                if($adr->type == 'billing_address')
+                {
+                    $billing_address = $adr;
+                }
+                if($adr->type == 'shipping_address')
+                {
+                    $shipping_address = $adr;
+                }
+                // $addressToSend[] = $adr;
             }
-            $address = AddresModel::where('user_id',$initial_order->user_id)->where('address_type','both')->first();
+            $address = AddresModel::where('user_id', $initial_order->user_id)->where('address_type', 'both')->first();
             $order = [
-                'order_id'=> $request->order_id,
-                'order_status'=> $initial_order->status,
-                'order_date'=> date('M d , Y',strtotime($initial_order->created_at)),
-                'order_method'=>$initial_order->method,
-                'total_amount'=>$initial_order->amount,
-                'address'=>$addressToSend,
+                'order_id' => $request->order_id,
+                'order_status' => $initial_order->status,
+                'order_date' => date('M d , Y', strtotime($initial_order->created_at)),
+                'order_method' => $initial_order->method,
+                'total_amount' => $initial_order->amount,
+                'shipping_address' => $shipping_address,
+                'shipping_address' => $shipping_address,
             ];
 
             $output['res'] = 'success';
@@ -148,7 +154,6 @@ class OrdersController extends Controller
             $output['order_details'] = $order;
             $output['data'] = $collection_arr;
             return response()->json($output, 200);
-
         }
     }
 }
