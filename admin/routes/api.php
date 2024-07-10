495a6af1f33caee19c8947c00dc4dd66b88a8e22
shubham sharma <shubham.swapit@gmail.com>
2024-07-10 12:14:48 +0530

    changed api method

--- admin/routes/api.php
+++ admin/routes/api.php
@@ -68,7 +68,7 @@
 		Route::get('check-postal-code/{code}',[App\Http\Controllers\API\CheckOutController::class,'checkValidPosalCode']);
 		Route::get('coveted-products/{type}',[App\Http\Controllers\API\ProductController::class,'covetedProducts']);
         Route::get('/invoice/{order_id}',[App\Http\Controllers\InvoiceController::class, 'invoicePdf'])->name('sale.orders.invoice.download');
-        Route::get('meta_data', [App\Http\Controllers\API\MenuController::class, 'getMetaData']);
+        Route::post('meta_data', [App\Http\Controllers\API\MenuController::class, 'getMetaData']);
 
 	});
 
